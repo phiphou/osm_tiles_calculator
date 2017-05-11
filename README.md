5d5f6d3c0e15e29512a30f9b2fe97b8dfd329aed
Phiphou <phiphou@phiphou.com>
2017-05-11 11:59:20 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# osm_tiles_calculator
-Calculate the amount of tiles needed for a given area for an OpenStreetMap map.
+[![npm](https://img.shields.io/npm/v/osm_tiles_calculator.svg)](https://www.npmjs.com/package/osm_tiles_calculator)
+[![Dependency Status](https://david-dm.org/phiphou/osm_tiles_calculator.svg)](https://david-dm.org/phiphou/osm_tiles_calculator)
+[![MIT license](https://img.shields.io/badge/Licence-MIT-blue.svg)](http://opensource.org/licenses/MIT)
+[![Size](https://reposs.herokuapp.com/?path=phiphou/osm_tiles_calculator)](#)
+
+[![NPM](https://nodei.co/npm/osm_tiles_calculator.png?downloads=true&stars=true)](https://nodei.co/npm/osm_tiles_calculator/)
+
+
+# Osm Tiles calculator
+
+Calculte the amount of tiles needs for a given area and an optional zooms range.
+
+
+## Install
+
+With [npm](https://npmjs.org):
+
+```
+$ npm i osm_tiles_calculator -g
+```
+
+## Usage
+
+```
+$ osm_tiles_calculator -b -2 45 3 48
+```
+Try
+
+```
+$ osm_tiles_calculator --help for detailled usage instructions.
+
+```
+## Options
+
+##### `-bounds` ( `-b` ) (required)
+
+Bounds of the area for witch you want to calculate the tiles needed amount.
+
+Bounds must be in the form *minLon minLat maxLon maxLat*
+
+Latitudes must be lower that 85° and greater than -85°.
+
+Example :
+```
+$ osm_tiles_calculator -b -2 45 3 48
+```
+
+##### `-zooms` ( `-z` ) (optional)
+
+An optional option you can use to specify zoom or zoom range for the calculation. Default is range [0-20]
+
+```
+$ osm_tiles_calculator -b -2 45 3 48 -z 13
+```
+
+or
+
+```
+$ osm_tiles_calculator -b -2 45 3 48 -z 13 15
+```
+
+##### `-output` ( `-o` ) (optional)
+
+An optional option you can use to generate a .txt file containing the tiles count report.
+
+```
+$ osm_tiles_calculator -b -2 45 3 48 -o
+```
+You can also specify the filename for the generated file :
+
+```
+$ osm_tiles_calculator -b -2 45 3 48 -o my_file
+```
+
+
+## License
+
+osm_tiles_calculator is available under the [MIT license](https://tldrlegal.com/license/mit-license).
+
+
+## Contact
+
+Copyright (C) 2017 Phiphou
+
+[![@phiphou](https://img.shields.io/badge/github-phiphou-green.svg)](https://github.com/phiphou) [![@__phiphou__](https://img.shields.io/badge/twitter-__phiphou__-blue.svg)](https://twitter.com/__phiphou__)
---
+++ bin/index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const utils = require('../lib/utils')
+
+var argv = require('yargs')
+  .usage('Usage: <command> [options]')
+  .command('tilescalculator', 'Calculate tiles amount needed for a zoom level or range.')
+  .example('tilescalculator -bounds -zooms', 'Calculate tiles amount needed for a zoom level or range.')
+  .option('bounds', {
+    alias: 'b',
+    demandOption: true,
+    describe: 'bounds in the form "lat1 lon1 lat2 lon2".',
+    type: 'array'
+  })
+  .option('zooms', {
+    alias: 'z',
+    default: [0, 20],
+    describe: 'zoom levels in the form "min_zom max_zoom".',
+    type: 'array'
+  })
+  .option('out', {
+    alias: 'o',
+    describe: 'Write report in a .txt file. Default is tiles_count.txt',
+    type: 'string'
+  })
+  .string('o')
+  .check(check_args)
+  .showHelpOnFail(false, 'Specify --help or -h for usage instructions')
+  .help('h')
+  .alias('h', 'help')
+  .epilog('copyright Phiphou 2017')
+  .argv
+
+function check_args(args, options) {
+  check_bounds(args.bounds)
+  check_zooms(args.zooms)
+  return true
+}
+
+function check_bounds(bounds) {
+  if (bounds.length == 0)
+    throw "bounds must be specified with --bounds or -b."
+  if (bounds.length != 4)
+    throw "bounds must be defined with 4 space separated values."
+  for (var i = 0; i < bounds.length; i++)
+    if (typeof (bounds[i]) != 'number')
+      throw "bounds must be defined with 4 numbers."
+  if (bounds[0] > bounds[2])
+    throw "lat1 must be lower than lat2."
+  if (bounds[1] > bounds[3])
+    throw "lon1 must be lower than lon2."
+  if (bounds[0] < -85)
+    throw "lat1 must be lower than -85°."
+  if (bounds[2] > 85)
+    throw "lat1 must be lower than 85°."
+}
+
+function check_zooms(zooms) {
+  if (zooms.length == 0)
+    throw "bounds must be specified with --zooms or -z."
+  if (zooms.length != 2)
+    throw "bounds must be defined with 2 space separated values."
+  if (zooms[0] > zooms[1])
+    throw "zoom1 must be lower than zoom2."
+  if (zooms[0] < 0)
+    throw "zooms1 must be greater than or equal to 0."
+  if (zooms[1] > 22)
+    throw "zooms2 must be lower than 22."
+}
+
+function tiles4zooms(bounds, minZ, maxZ = minZ) {
+  let total = 0
+  let output = `\r\nbounds: ${bounds}\r\nzoom(s): ${minZ}-${maxZ}\r\n\r\n`
+  let i
+  for (i = minZ; i <= maxZ; i++) {
+    let count = utils.tiles4zoom(bounds, i)
+    count = count == 0 ? 1 : count
+    output += `zoom ${i}: ${utils.number_format(count)}\r\n`
+    total += count
+  }
+  output += `\r\ntotal: ${utils.number_format(total)}`
+  console.log(output)
+  output = `Tiles Calculator\r\n${output}`
+  if (argv.hasOwnProperty('o')) {
+    let filename = argv.out || 'tiles_count'
+    fs.writeFileSync(`${filename}.txt`, output, 'utf8')
+  }
+}
+
+tiles4zooms(argv.bounds, argv.zooms[0], argv.zooms[1])
